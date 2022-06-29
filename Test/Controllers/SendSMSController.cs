ba8f661be3456e2677db95959309904a62f1e4e8
pribyto4 <tomas.pribyl.89@gmail.com>
2022-06-29 16:33:30 +0200

    #2 hotová validace PhoneNumber

--- Test/Controllers/SendSMSController.cs
+++ Test/Controllers/SendSMSController.cs
@@ -16,13 +16,26 @@ public class SendSMSController : Controller
     [HttpGet]
     public IActionResult Index(SendSMS obj)
     {
-        if (obj.PhoneNumber != null)
-        { //write data in File
-            string FilePath = "/media/tom/rychlý data/Dokumenty/Tomáš/PRG Linux/Programy - .NET/Test4_4 MVC dotnet6.0 generovaný/Test/Data/SendSMS.txt";
-            object Text = obj.PhoneNumber + "," + obj.SMSText;
-            Helpers.FileUtil.AddLine(FilePath, Text);
+        if (ModelState.IsValid)
+        {
+            if (obj.PhoneNumber != null)
+            {
+                if (obj.PhoneNumber[0] == '+')
+                {
+                    obj.PhoneNumber = obj.PhoneNumber.Substring(1, obj.PhoneNumber.Length-1);
+                }
+                //write data in File
+                string FilePath = "/media/tom/rychlý data/Dokumenty/Tomáš/PRG Linux/Programy - .NET/Test4_4 MVC dotnet6.0 generovaný/Test/Data/SendSMS.txt";
+                object Text = obj.PhoneNumber + "," + obj.SMSText;
+                Helpers.FileUtil.AddLine(FilePath, Text);
+            }
+
+            return View(obj);
         }
-        return View(obj);
+        else
+        {
+            return View();
+        }
     }
 
 }
--- Test/Models/SendSMS.cs
+++ Test/Models/SendSMS.cs
@@ -6,13 +6,13 @@ namespace Test.Models
     public class SendSMS
     {
         [Display(Name = "Phone number (with Country prefix)")]
-        [RegularExpression(@"[0-9]+", ErrorMessage = "Must be digits.")] //nefunguje
-        [MaxLength(15, ErrorMessage = "Maximum length is 15")]
+        [RegularExpression(@"[0-9]+|^\+[0-9]+", ErrorMessage = "Must be digits.")]
+        [MaxLength(16, ErrorMessage = "Maximum length is 16")]
         public string? PhoneNumber { get; set; } = null;
 
         [Display(Name = "SMS text")]
         [StringLength(320)]
-        [RegularExpression(@"\x00-\x7F")] //mělo by být povolení jen Ascii7, nefunguje
+       // [RegularExpression(@"\x00-\x7F")] //mělo by být povolení jen Ascii7, nefunguje
         public string? SMSText { get; set; }
     }
 }
